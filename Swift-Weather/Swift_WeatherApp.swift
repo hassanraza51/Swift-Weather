2a35e36bfce81d40e08fd2cefed98a793a18a9fb
Hassan Raza <razahassan616@gmail.com>
2023-09-14 21:39:24 +0500

    Initial Commit

---
+++ Swift-Weather/Swift_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WeatherApp.swift
+//  Swift-Weather
+//
+//  Created by Hassan Raza on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
